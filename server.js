66c0c3c46f7ab545116f7f832badbca6323c3256
Roy Huang <Roy.Huang@navico.com>
2022-01-30 09:40:51 +1300

    Support https

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
-const path = require('path');
+const key = fs.readFileSync(path.join(__dirname, 'certs', 'signed', 'key.pem'));
+const cert = fs.readFileSync(path.join(__dirname, 'certs', 'signed', 'cert.pem'));
+
+const options = {
+    key: key,
+    cert: cert
+};
+
+
 const PORT = process.env.PORT || 3500;
 
 app.use(express.static('Tetris-js'));
@@ -19,4 +31,6 @@ app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+let server = https.createServer(options, app);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
